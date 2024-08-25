cfd36e3ac270768d7832942923cd6dce3f34c9b1
ketansharma22 <kanu220504@gmail.com>
2024-08-26 00:18:21 +0530

    main

--- Animationn/src/App.jsx
+++ Animationn/src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Start from './screens/Start'
-import About from './screens/About'
+import "./App.css";
+import Start from "./screens/Start";
+import About from "./screens/About";
+import {Canvas} from '@react-three/fiber'
+import {OrbitControls} from '@react-three/drei'
+import {Bloom, EffectComposer} from '@react-three/postprocessing'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Canvas flat camera={{fov:25}}>
+      <OrbitControls enableZoom={false} />
+      <ambientLight />
       <Start/>
-      {/* <About/> */}
+      <EffectComposer>
+        <Bloom 
+        minmapBlur
+        intensity={0.5}
+        luminanceThreshold={0}
+        luminanceSmoothing={0} />
+      </EffectComposer>
+    </Canvas>
+    <div>helo</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Animationn/src/screens/Start.jsx
+++ Animationn/src/screens/Start.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import {Link} from 'react-scroll'
+import React, { useRef } from 'react'
+// import {Link} from 'react-scroll'
 import '../styling/Start.css'
+import { useTexture} from '@react-three/drei'
+import * as THREE from 'three'
+import imag from './imag.png'
+import { useFrame } from '@react-three/fiber'
 function Start() {
+  let tex=useTexture(imag)
+  let cyl=useRef(null)
+  useFrame((state,delta)=>{
+    cyl.current.rotation.y +=delta
+  })
   return (
-    <div id='start'>
-        {/* <Link spy={true} smooth={true} offset={-50} duration={500} to='about'>About</Link> */}
-
-        <h1>KETAN SHARMA</h1>
-    </div>
+    <group rotation={[0,1.4,0.5]}>
+      <mesh ref={cyl} >
+        <cylinderGeometry args={[1,1,1,10,10,true]} />
+        <meshStandardMaterial transparent map={tex} side={THREE.DoubleSide} />
+      </mesh>
+      </group>
+  
   )
 }
 
